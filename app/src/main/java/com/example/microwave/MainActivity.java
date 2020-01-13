947ead6505d98d1fabff9d69f8eb8105b682f743
fwteinhpap <34911926+fwteinhpap@users.noreply.github.com>
2020-01-13 20:22:13 +0200

    fix not swiping tabs

--- app/src/main/java/com/example/microwave/MainActivity.java
+++ app/src/main/java/com/example/microwave/MainActivity.java
@@ -1,36 +1,72 @@
 package com.example.microwave;
 
+import android.content.DialogInterface;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.design.widget.TabLayout;
 import android.support.v4.view.ViewPager;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
-
+import android.widget.Button;
+import android.widget.Toast;
+import com.example.microwave.ui.main.CustomViewPager;
 import com.example.microwave.ui.main.SectionsPagerAdapter;
 
 public class MainActivity extends AppCompatActivity {
-
+    CustomViewPager viewPager;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         SectionsPagerAdapter sectionsPagerAdapter = new SectionsPagerAdapter(this, getSupportFragmentManager());
-        ViewPager viewPager = findViewById(R.id.view_pager);
+        viewPager = findViewById(R.id.view_pager);
         viewPager.setAdapter(sectionsPagerAdapter);
         TabLayout tabs = findViewById(R.id.tabs);
         tabs.setupWithViewPager(viewPager);
+
+        for (View v: tabs.getTouchables())
+            v.setClickable(false);
         FloatingActionButton fab = findViewById(R.id.fab);
 
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+                AlertDialog.Builder builder1 = new AlertDialog.Builder(view.getContext());
+                builder1.setMessage("Write your message here.");
+                builder1.setCancelable(true);
+
+                builder1.setNeutralButton(
+                        "OK",
+                        new DialogInterface.OnClickListener() {
+
+                            public void onClick(DialogInterface dialog, int id) {
+                                dialog.cancel();
+                            }
+                        });
+
+
+
+                AlertDialog alert11 = builder1.create();
+
+                alert11.show();
+                alert11.getButton(AlertDialog.BUTTON_NEUTRAL).setTextColor(getResources().getColor(R.color.colorPrimary));
             }
         });
+
+
     }
+
+    public ViewPager getViewPager() {
+        if (null == viewPager) {
+            viewPager =  findViewById(R.id.view_pager);
+        }
+        return viewPager;
+    }
+
+
 }
--- app/src/main/java/com/example/microwave/Tab1.java
+++ app/src/main/java/com/example/microwave/Tab1.java
@@ -4,11 +4,19 @@ import android.arch.lifecycle.Observer;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
+import android.support.design.widget.Snackbar;
+import android.support.design.widget.TabLayout;
 import android.support.v4.app.Fragment;
+import android.support.v4.view.ViewPager;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import com.example.microwave.ui.main.SectionsPagerAdapter;
 
 public class Tab1 extends Fragment {
     @Override
@@ -16,6 +24,15 @@ public class Tab1 extends Fragment {
             @NonNull LayoutInflater inflater, ViewGroup container,
             Bundle savedInstanceState) {
         View root = inflater.inflate(R.layout.tab1, container, false);
+        final SectionsPagerAdapter sectionsPagerAdapter = new SectionsPagerAdapter(getContext(), getActivity().getSupportFragmentManager() );
+        final Button button = root.findViewById(R.id.button_start);
+        button.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                ((MainActivity)getActivity()).getViewPager().setCurrentItem(1);
+            }
+        });
         return root;
     }
+
+
 }
