555826a91e0c7449ad190f49132d60294b8a4d29
Hilsen-Cenaj <58752665+Hilsen-Cenaj@users.noreply.github.com>
2020-01-14 17:43:56 +0200

    Tab3 - adding buttons texts etc

--- app/src/main/java/com/example/microwave/Tab3.java
+++ app/src/main/java/com/example/microwave/Tab3.java
@@ -1,13 +1,16 @@
 package com.example.microwave;
 
+import android.content.DialogInterface;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
+import android.support.v7.app.AlertDialog;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 public class Tab3 extends Fragment {
 
@@ -15,6 +18,42 @@ public class Tab3 extends Fragment {
             @NonNull LayoutInflater inflater, ViewGroup container,
             Bundle savedInstanceState) {
         View root = inflater.inflate(R.layout.tab3, container, false);
+
+        final Button button_finalS = root.findViewById(R.id.button_final_start);
+        final Button button_toTab2 = root.findViewById(R.id.button_to_tab2);
+
+        button_finalS.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                AlertDialog.Builder builder = new AlertDialog.Builder(view.getContext());
+                builder.setMessage("Write your message here.");
+                builder.setCancelable(true);
+
+                builder.setNeutralButton(
+                        "OK",
+                        new DialogInterface.OnClickListener() {
+
+                            public void onClick(DialogInterface dialog, int id) {
+                                dialog.cancel();
+                            }
+                        });
+
+
+
+                AlertDialog alert11 = builder.create();
+
+                alert11.show();
+                alert11.getButton(AlertDialog.BUTTON_NEUTRAL).setTextColor(getResources().getColor(R.color.colorPrimary));
+            }
+        });
+
+        button_toTab2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                ((MainActivity)getActivity()).getViewPager().setCurrentItem(1);
+            }
+        });
 
         return root;
     }
